94dd6f5e66bfb05ce7a501cd89554376f2e19483
Balakrishnan Balasubramanian <bbalasub@bbalasub.sin.csb>
2020-04-09 12:54:08 +0800

    added docker compose

--- kafka-quickstart/src/main/java/com/redhat/TwitResource.java
+++ kafka-quickstart/src/main/java/com/redhat/TwitResource.java
@@ -1,23 +1,26 @@
-package org.acme;
-
-import io.smallrye.reactive.messaging.annotations.Channel;
-import io.smallrye.reactive.messaging.annotations.Emitter;
-import org.reactivestreams.Publisher;
+package com.redhat;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import org.jboss.resteasy.annotations.SseElementType;
+import org.reactivestreams.Publisher;
+import io.smallrye.reactive.messaging.annotations.Channel;
+import io.smallrye.reactive.messaging.annotations.Emitter;
 
 @Path("twit")
 public class TwitResource {
 
-    @Inject @Channel("twit-out") Emitter<String> emitter;
-    @Inject @Channel("twit-in") Publisher<String> txt; 
+    @Inject
+    @Channel("twit-out")
+    Emitter<String> emitter;
+    @Inject
+    @Channel("twit-in")
+    Publisher<String> txt;
 
     @POST
     @Consumes(MediaType.TEXT_PLAIN)
@@ -28,14 +31,13 @@ public class TwitResource {
 
     @GET
     @Path("/stream")
-    @Produces(MediaType.SERVER_SENT_EVENTS) 
-    @SseElementType("text/plain") 
-    public Publisher<String> stream() { 
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType("text/plain")
+    public Publisher<String> stream() {
         System.out.println("recieved: " + txt);
         return txt;
     }
 
 }
-
 
 
